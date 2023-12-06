020fb77ca965c69719931a4e06e5c1fcf3c7feff
jakubkanna <hello.jakubkanna@gmail.com>
2023-12-06 13:48:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Title
+
+**Note:** Description
+
+## The Odin Project
+
+### Project: Title
+
+#### Name of the Course
+
+##### Assignment
+
+## Live Preview
+
+[Add Desktop Design Image/Link Here]()
+
+### Desktop Design
+
+[Add Desktop Design Image/Link Here]()
+
+### Mobile Design
+
+[Add Mobile Design Image/Link Here]()
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "docs"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./src",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
