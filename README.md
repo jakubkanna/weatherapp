c33417fd0eb0f6bb08181d35e5867818049456f8
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-06 17:39:44 +0100

    fetches data from the server

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
-# Title
+# Weather App
 
-**Note:** Description
+**Note:** Yes you don't see a stylesheet for a reason! The goal is to create good design and the app using only default user agent stylesheet.
 
 ## The Odin Project
 
-### Project: Title
+### Project: Weather App
 
-#### Name of the Course
+#### JavaScript Course
 
 ##### Assignment
 
+- Set up a blank HTML document with the appropriate links to your JavaScript and CSS files.
+- Write the functions that hit the API. Create functions that can take a location and return the weather data for that location. For now, just use `console.log()` to display the information.
+- Write functions that process the JSON data you get from the API and return an object with only the data you require for your app.
+- Set up a simple form that lets users input their location and fetches the weather info (still just using `console.log()` for now).
+- Display the information on your webpage!
+- Add any styling you like!
+- _Optional:_ Add a 'loading' component that displays from the time the form is submitted until the information comes back from the API. Use DevTools to test for low-end devices.
+- Push your code to GitHub and share your solution below!
+
 ## Live Preview
 
-[Add Desktop Design Image/Link Here]()
+[Add Desktop Design Image/Link Here]
 
 ### Desktop Design
 
-[Add Desktop Design Image/Link Here]()
+[Add Desktop Design Image/Link Here]
 
 ### Mobile Design
 
-[Add Mobile Design Image/Link Here]()
+[Add Mobile Design Image/Link Here]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 import "./style.css";
+
+// Controller to coordinate data and UI
+const weatherController = {
+  //on form submit
+  handleFormSubmit: async function (cityName) {
+    try {
+      const data = await getWeather(cityName); //after data is ready
+      this.handleData(data);
+    } catch (error) {
+      this.handleError(error);
+    }
+  },
+  handleData: function (data) {
+    // Display the data
+    displayData(data);
+  },
+  handleError: function (error) {
+    console.error("Error in controller:", error);
+    // Handle the error in the UI if needed
+  },
+};
+
+// Get data
+async function getWeather(cityName) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=0c74e0d9f7ae4709b0f121149230512&q=${cityName}&days=7`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Caught error during fetch:", error);
+    throw error;
+  }
+}
+
+// Handle dom click
+function handleForm(controller) {
+  // Form
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const input = document.getElementById("_location");
+    const cityName = input.value.toString();
+
+    // Call the controller to handle form submission (data)
+    await controller.handleFormSubmit(cityName);
+  });
+}
+
+// Display data
+function displayData(data) {
+  console.log(data);
+}
+
+// When doc is ready, initialize the form handling with the controller
+document.addEventListener("DOMContentLoaded", () => {
+  handleForm(weatherController);
+});
