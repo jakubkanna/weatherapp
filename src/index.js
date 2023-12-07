8f96a51c5e264e4c80ea357b75cb4dc57f9e0b8b
jakubkanna <hello.jakubkanna@gmail.com>
2023-12-07 18:27:48 +0100

    Update index.js. Updated DOMGenerator

--- src/index.js
+++ src/index.js
@@ -77,49 +77,87 @@ class DOMGenerator {
     this.data = data;
   }
 
+  // Creates and returns a div element with a given key as its id
+  createDivElement(key) {
+    const element = document.createElement("div");
+    element.id = key;
+    return element;
+  }
+
+  // Inserts a header element into the parent with the specified key and level
   insertHeaders(parent, key, level) {
     const header = document.createElement(`h${level}`);
     header.textContent = key.charAt(0).toUpperCase() + key.slice(1);
     parent.appendChild(header);
   }
 
-  createElement(parent, key, value, level = 2) {
-    const element = document.createElement("div");
-    element.id = key;
-
-    this.insertHeaders(element, key, level);
-
-    if (typeof value === "object") {
-      for (const nestedKey in value) {
-        this.createElement(element, nestedKey, value[nestedKey], level + 1);
-      }
-    } else {
-      if (this.isImageURL(value)) {
-        const img = document.createElement("img");
-        img.src = value;
-        element.appendChild(img);
-      } else {
-        const span = document.createElement("span");
-        span.textContent = value;
-        element.appendChild(span);
-      }
+  // Creates nested elements for an object and appends them to the parent
+  createNestedElements(parent, nestedData, level) {
+    for (const nestedKey in nestedData) {
+      this.createElement(parent, nestedKey, nestedData[nestedKey], level);
     }
-
-    parent.appendChild(element);
   }
 
+  // Creates a leaf element (either an image or a span) and appends it to the parent
+  createLeafElement(parent, value) {
+    if (this.isImageURL(value)) {
+      this.createImageElement(parent, value);
+    } else {
+      this.createSpanElement(parent, value);
+    }
+  }
+
+  // Creates an image element with the specified source and appends it to the parent
+  createImageElement(parent, src) {
+    const img = document.createElement("img");
+    img.src = src;
+    parent.appendChild(img);
+  }
+
+  // Creates a span element with the specified text content and appends it to the parent
+  createSpanElement(parent, text) {
+    const span = document.createElement("span");
+    span.textContent = text;
+    parent.appendChild(span);
+  }
+
+  // Checks if the given URL is an image URL based on its extension
   isImageURL(url) {
     return /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(url);
   }
 
+  // Generates the entire DOM structure based on the provided data
   generateDOM() {
-    const rootElement = document.createElement("div");
-    rootElement.id = "root";
+    // Create the root element for the DOM structure
+    const rootElement = this.createDivElement("root");
 
+    // Iterate through the provided data and create elements accordingly
     for (const key in this.data) {
       this.createElement(rootElement, key, this.data[key]);
     }
 
     return rootElement;
   }
+
+  // Main function to create an element and its children recursively
+  createElement(parent, key, value, level = 2) {
+    // Create a div element for the current key
+    const element = this.createDivElement(key);
+
+    // Insert headers based on the key and level
+    this.insertHeaders(element, key, level);
+
+    // Check if the value is an object (nested structure) or not
+    if (typeof value === "object") {
+      // If it's an object, create nested elements
+      this.createNestedElements(element, value, level + 1);
+    } else {
+      // If it's not an object, create a leaf element
+      this.createLeafElement(element, value);
+    }
+
+    // Append the created element to the parent
+    parent.appendChild(element);
+  }
 }
+
