32134592f3fb5520867abe5a023812bd31f76a3d
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-11 12:21:23 +0100

    build, cleanup

--- src/index.js
+++ src/index.js
@@ -1,127 +1,129 @@
-import "./style.css";
-import { HomeGen, GifGen } from "./index_modules/dom/home-generator";
-import { DataToggler, BasicToggler } from "./index_modules/dom/togglers";
+// domModule.js
+import { BasicToggler, DataToggler } from "./index_modules/dom/togglers.js";
+import { HomeGen } from "./index_modules/dom/home-generator.js";
+
+export const domModule = {
+  // Handle form events
+  handleForm: function () {
+    const form = document.getElementById("cityName");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const input = document.getElementById("_location");
+      const cityName = input.value.toString();
+      controller.handleForm(cityName);
+    });
+  },
+  // Handle menu buttons
+  handleMenuButtons: function () {
+    new BasicToggler("tempToggler", '[id*="_c"]', '[id*="_f"]');
+    new DataToggler("detailToggler", "#root *");
+  },
+  // Get condition elements
+  get conditionEls() {
+    return document.querySelectorAll("#condition");
+  },
+  // Append generated DOM to the main element
+  appendDOM: function (generatedDOM) {
+    const main = document.body.querySelector("main");
+    main.innerHTML = ""; // Clear existing content
+    main.appendChild(generatedDOM);
+  },
+  createGIFs: function () {
+    this.conditionEls.forEach(async function (element) {
+      const text = element.querySelector("#text").innerHTML.toString();
+      const src = await controller.handleGif(text);
+      const icon = element.querySelector("#icon");
+      const placeholder = element.querySelector("#placeholder");
+
+      // Create an image element for the GIF
+      function createIcon(src) {
+        const img = document.createElement("img");
+        img.src = src;
+        return img;
+      }
+
+      const img = createIcon(src);
+      placeholder.remove(); // Remove "Loading..."
+      icon.append(img);
+    });
+  },
+  // Create generated DOM
+  createDOM: function (data) {
+    const dom = new HomeGen(data).generateDOM();
+    this.appendDOM(dom);
+  },
+};
+
+// dataModule.js
 import DataFetcher from "./index_modules/data/fetcher.js";
 import modifyForecastData from "./index_modules/data/modifier.js";
 
-const data = {
+export const dataModule = {
   fetcher: new DataFetcher(),
-  async getForecast(cityName) {
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=0c74e0d9f7ae4709b0f121149230512&q=${cityName}&days=7`;
-
-    // Display loading state
+  // Get forecast data
+  getForecast: async function (cityName) {
     controller.displayLoading();
 
     try {
-      const weatherData = await this.fetcher.getData(url);
+      const url = `https://api.weatherapi.com/v1/forecast.json?key=0c74e0d9f7ae4709b0f121149230512&q=${cityName}&days=7`;
+      const weatherData = await dataModule.fetcher.getData(url);
       const newWeatherData = modifyForecastData(weatherData);
-
-      // Save data to localStorage
       localStorage.setItem("newWeatherData", JSON.stringify(newWeatherData));
-
       return newWeatherData;
     } catch (error) {
       console.error(error);
     }
   },
-  async getGif(query) {
-    const url = `https://api.giphy.com/v1/gifs/translate?api_key=t9evMNjWyyMw8FgM8GfNFAG8TkuYUklE&s=${query} weather'`;
+  // Get Giphy data
+  getGif: async function (query) {
     try {
-      const giphyData = await this.fetcher.getData(url);
-      const giphyURL = giphyData.data.images.fixed_height.url;
-
-      return giphyURL;
+      const url = `https://api.giphy.com/v1/gifs/translate?api_key=t9evMNjWyyMw8FgM8GfNFAG8TkuYUklE&s=${query} weather'`;
+      const giphyData = await dataModule.fetcher.getData(url);
+      return giphyData.data.images.fixed_height.url;
     } catch (error) {
       console.error(error);
     }
   },
 };
 
-const dom = {
-  //handle events
-  handleForm() {
-    const form = document.getElementById("cityName");
-
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      const input = document.getElementById("_location");
-      const cityName = input.value.toString();
-
-      // pass to controller
-      controller.formHandler(cityName);
-    });
-  },
-  handleMenuButtons() {
-    // Create button to toggle visibility of [id^="_c"] and [id*="_f"]
-    new BasicToggler("tempToggler", '[id*="_c"]', '[id*="_f"]');
-    new DataToggler("detailToggler", "#root *");
-  },
-  get conditionEls() {
-    return document.querySelectorAll("#condition");
-  },
-  appendDOM(generatedDOM) {
-    const main = document.body.querySelector("main");
-    main.innerHTML = ""; // Clear existing content
-    main.appendChild(generatedDOM);
-  },
-};
-
-const controller = {
-  init() {
-    //handle
-    dom.handleForm();
-    // Check if there's data in localStorage when the page loads
+// controller.js
+export const controller = {
+  // Initialize the controller
+  init: function () {
+    domModule.handleForm();
     const storedForecastData = localStorage.getItem("newWeatherData");
     if (storedForecastData) {
       const parsedData = JSON.parse(storedForecastData);
-      this.displayForecast(parsedData);
-      this.displayGifs();
+      // Display forecast and GIFs if data is available in localStorage
+      controller.displayForecast(parsedData);
     }
   },
-
-  displayForecast(data) {
-    const generatedDOM = new HomeGen(data).generateDOM();
-
-    dom.appendDOM(generatedDOM);
-
-    dom.handleMenuButtons(); //do it after new dom is loaded
+  // Display forecast data
+  displayForecast: function (data) {
+    domModule.createDOM(data);
+    domModule.handleMenuButtons();
+    domModule.createGIFs();
   },
-  displayGifs() {
-    dom.conditionEls.forEach(async (element) => {
-      const text = element.querySelector("#text").innerHTML.toString();
-      const icon = element.querySelector("#icon");
-      const placeholder = element.querySelector("#placeholder");
 
-      const gif = await data.getGif(`${text} weather`);
-
-      function createIcon(gif) {
-        const img = document.createElement("img");
-        img.src = gif;
-        return img;
-      }
-
-      const img = createIcon(gif);
-      placeholder.remove();
-      icon.append(img);
-    });
+  // Handle GIF fetching
+  handleGif: async function (text) {
+    const gif = await dataModule.getGif(`${text} weather`);
+    return gif;
   },
-  async formHandler(cityName) {
-    const weather = await data.getForecast(cityName);
-    console.log(weather);
-
-    // Display skeleton
-    this.displayForecast(weather);
-
-    // Insert images to skeleton
-    this.displayGifs();
+  // Handle form submission
+  handleForm: async function (cityName) {
+    const weather = await dataModule.getForecast(cityName);
+    controller.displayForecast(weather);
   },
-  displayLoading() {
-    // Update your UI to display a loading state, e.g., show a spinner or a message
+  // Display loading state
+  displayLoading: function () {
     console.log("Loading...");
   },
 };
 
-// When dom is ready init contrl.
-document.addEventListener("DOMContentLoaded", () => {
+// main.js
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", function () {
   controller.init();
 });
